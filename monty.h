740b10372c355550cc05d0af79be523c4d81cab1
memo-alxroid <142922525+memo-alxroid@users.noreply.github.com>
2024-01-01 04:28:08 +0200

    contains all functions declaration and structs

--- monty.h
+++ monty.h
@@ -1,12 +1,15 @@
 #ifndef MONTY_H_
 #define MONTY_H_
+#define _POSIX_C_SOURCE 200809L
 
 #include <stdio.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -24,7 +27,6 @@ typedef struct stack_s
         struct stack_s *next;
 } stack_t;
 
-
 /**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
@@ -38,5 +40,17 @@ typedef struct instruction_s
         char *opcode;
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+
+FILE *open_file_if_exist(const char *fileName);
+void read_commands_from_file(char *commands, FILE *filePointer);
+void (*get_op_function(char *opcode))(stack_t **stack, unsigned int line_number);
+void execute_command_if_exist(char *command, stack_t **stack, unsigned int lineNumber);
+void pushOP(stack_t **stack, unsigned int line_number);
+void pallOP(stack_t **stack, unsigned int line_number);
+void pintOP(stack_t **stack, unsigned int line_number);
+void popOP(stack_t **stack, unsigned int line_number);
+void swapOP(stack_t **stack, unsigned int line_number);
+void addOP(stack_t **stack, unsigned int line_number);
+void nopOP(stack_t **stack, unsigned int line_number);
 
 #endif
