de9bf70b0a22dd489f6d80ffe3ac3705edec410a
memo-alxroid <142922525+memo-alxroid@users.noreply.github.com>
2024-01-01 04:25:49 +0200

    removes the top element of the stack

---
+++ pop.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+
+/**
+ * popOP - remove the top element of the stack
+ * @stack: stack
+ * @line_number: line number
+ * Return: void
+ */
+
+void popOP(stack_t **stack, unsigned int line_number)
+{
+	stack_t *currentElement = (*stack);
+
+	if (currentElement == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		(*stack) = currentElement->next;
+		free(currentElement);
+	}
+}
