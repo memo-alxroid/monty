7d008b26767692ffdec5bf23d636ae4dcd1ecedf
memo-alxroid <142922525+memo-alxroid@users.noreply.github.com>
2024-01-01 04:26:07 +0200

    pushes an element to the stack

---
+++ push.c
@@ -0,0 +1,36 @@
+#include "monty.h"
+
+void pushOP(stack_t **stack, unsigned int line_number)
+{
+	stack_t *currentElement = (*stack);
+	stack_t *newElement = NULL;
+
+	if (line_number == 0)
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	newElement = malloc(sizeof(stack_t));
+	if (newElement == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	newElement->n = line_number;
+
+	if (currentElement == NULL)
+	{
+		newElement->prev = NULL;
+		newElement->next = NULL;
+		(*stack) = newElement;
+	}
+	else
+	{
+		newElement->prev = NULL;
+		newElement->next = currentElement;
+		currentElement->prev = newElement;
+		(*stack) = newElement;
+	}
+}
