2d8fc025cb6913c7a290d9f21e2076daa405229a
memo-alxroid <142922525+memo-alxroid@users.noreply.github.com>
2024-01-01 04:22:36 +0200

    gets the operation code function

---
+++ get_op_function.c
@@ -0,0 +1,30 @@
+#include "monty.h"
+
+/**
+ * get_op_function - get opcode function
+ * @opcode: opcode to check
+ * Return: function to opcode
+ */
+
+void (*get_op_function(char *opcode))(stack_t **stack, unsigned int line_number)
+{
+	instruction_t opcodes[] = {
+		{"push", pushOP},
+		{"pall", pallOP},
+		{"pint", pintOP},
+		{"pop", popOP},
+		{"swap", swapOP},
+		{"add", addOP},
+		{"nop", nopOP},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (opcodes[i].opcode != NULL)
+	{
+		if (strcmp(opcode, opcodes[i].opcode) == 0)
+			return (opcodes[i].f);
+		i++;
+	}
+	return (NULL);
+}
