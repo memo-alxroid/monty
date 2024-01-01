591696903833906717435df507d8a1c1bbc80dcf
memo-alxroid <142922525+memo-alxroid@users.noreply.github.com>
2024-01-01 04:24:31 +0200

    checks if a file exist and then try to open it

---
+++ open_file_if_exist.c
@@ -0,0 +1,27 @@
+#include "monty.h"
+
+/**
+ * open_file_if_exist - open file if exist
+ *
+ * @filename: file name
+ *
+ * Return: file pointer or NULL
+ */
+
+FILE *open_file_if_exist(const char *fileName)
+{
+	FILE *filePointer;
+
+	if (fileName == NULL)
+	{
+		perror("USAGE: monty file");
+		exit(EXIT_FAILURE);
+	}
+	filePointer = fopen(fileName, "r");
+	if (filePointer == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", fileName);
+		exit(EXIT_FAILURE);
+	}
+	return (filePointer);
+}
