08804dd65d714b37d33a61e36e01b2abaf6fda87
memo-alxroid <142922525+memo-alxroid@users.noreply.github.com>
2024-01-01 04:23:57 +0200

    doesn’t do anything

---
+++ nop.c
@@ -0,0 +1,16 @@
+#include "monty.h"
+
+/**
+ * nopOP - does nothing
+ *
+ * @stack: stack
+ * @line_number: line number
+ *
+ * Return: void
+ */
+
+void nopOP(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
