4f4a5a801807dfd05f9bb8a6ef1ab8d64eb59e44
memo-alxroid <142922525+memo-alxroid@users.noreply.github.com>
2024-01-02 02:55:30 +0200

    Update read_commands_from_file.c

--- read_commands_from_file.c
+++ read_commands_from_file.c
@@ -9,20 +9,19 @@
  *
  */
 
-void read_commands_from_file(FILE *filePointer)
+void read_commands_from_file(FILE *filePointer, stack_t **stack)
 {
 	int lineNumber = 1;
 	char *command = NULL;
 	char *line = NULL;
 	size_t len = 0;
-	stack_t *stack = NULL;
-
+	
 	while (getline(&line, &len, filePointer) != -1)
 	{
 		command = strtok(line, " \n\t\r$");
 		if (command != NULL)
 		{
-			execute_command_if_exist(command, &stack, lineNumber);
+			execute_command_if_exist(command, stack, lineNumber);
 		}
 		lineNumber++;
 	}
