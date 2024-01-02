a42afa4c0e49a0363a8b037fd15250c253554e09
memo-alxroid <142922525+memo-alxroid@users.noreply.github.com>
2024-01-02 03:23:52 +0200

    Add files via upload

--- read_commands_from_file.c
+++ read_commands_from_file.c
@@ -9,21 +9,30 @@
  *
  */
 
-void read_commands_from_file(FILE *filePointer, stack_t **stack)
+void read_commands_from_file(FILE *filePointer)
 {
 	int lineNumber = 1;
-	char *command;
+	char *command = NULL;
 	char *line = NULL;
 	size_t len = 0;
+	stack_t *stack = NULL;
 
 	while (getline(&line, &len, filePointer) != -1)
 	{
 		command = strtok(line, " \n\t\r");
 		if (command != NULL)
 		{
-			execute_command_if_exist(command, stack, lineNumber);
+			execute_command_if_exist(command, &stack, lineNumber);
 		}
 		lineNumber++;
+	}
+	if (!feof(filePointer))
+	{
+		fclose(filePointer);
+		free(stack);
+		free(line);
+		printf("Error while reading from file\n");
+		exit(EXIT_FAILURE);
 	}
 	if (line != NULL)
 		free(line);
